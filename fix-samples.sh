54f6434b71a5814d69f1e1e1048a252857ffeb67
Osama Mansour <113166035+OsamaMansour25@users.noreply.github.com>
2023-02-06 13:37:36 +0000

    Opgave done

---
+++ fix-samples.sh
@@ -0,0 +1,8 @@
+ cd $CODESPACE_VSCODE_FOLDER/samples/
+ mv animals/countries/ .
+ mv countries/movies/ .
+ mv animals/Monkies.txt animals/Monkeys.txt 
+ echo Monkeys > animals/Monkeys.txt 
+ mv movies/Germany.txt countries/
+ mv countries/.apes.txt animals/
+ cd -
